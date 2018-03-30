a328008a40b39e9dedb7d8f4f463bdf38d492122
MB <buschman@umich.edu>
2018-03-31 01:28:40 +0200

    Add files via upload
    Works (I think) for two templates & two energy bins

--- NPTFit/npll.pyx
+++ NPTFit/npll.pyx
@@ -13,12 +13,15 @@
 #
 ###############################################################################
 
+import sys
 import numpy as np
 cimport numpy as np
 cimport cython
 import x_m
 import scipy.integrate as integrate
 from scipy.misc import factorial
+from scipy.special import binom
+from libc.stdlib cimport malloc, free
 
 # Type used for all non-integer functions
 DTYPE = np.float
@@ -53,22 +56,24 @@ def log_like(pt_sum_compressed, theta, f_ary, df_rho_div_f_ary, npt_compressed,
 
     """
 
+    cdef int nTheta = len(theta)
+    cdef int nBins = len(theta[0,-3]) - 1
+
     cdef int[::1] data_sum = np.array(map(sum,zip(*data)),dtype=np.int32)
-
+    cdef int[::1] k_max_bin = np.array(map(max,data+1),dtype=np.int32) 		
     cdef int k_max = np.max(data_sum) + 1
-    cdef int npixROI = len(pt_sum_compressed)
-    cdef double[:,::1] x_m_ary = np.zeros((npixROI,k_max + 1), dtype=DTYPE)
-    cdef double[::1] x_m_sum = np.zeros(npixROI, dtype=DTYPE)
+    cdef int k_max_comb = np.max(k_max_bin)
 
-    cdef Py_ssize_t i
+    cdef int npixROI = len(pt_sum_compressed)
+    cdef double[:,:,::1] x_m_ary = np.zeros((nTheta,npixROI,k_max + 1), dtype=DTYPE)
+    cdef double[:,::1] x_m_sum = np.zeros((nTheta,npixROI), dtype=DTYPE)
+
+    cdef Py_ssize_t i=0,b=0
     cdef double[:,::1] x_m_ary_out = np.zeros((npixROI,k_max + 1), dtype=DTYPE)
     cdef double[::1] x_m_sum_out = np.zeros(npixROI, dtype=DTYPE)
 
-    cdef int nBinsMax = np.max(map(len,theta[:,-3])) - 1
-    cdef int nTheta = len(theta)
-    cdef int[::1] nBins = np.zeros(nTheta,dtype=np.int32)
     cdef double[::1] norm = np.zeros(nTheta, dtype=DTYPE)
-    cdef double[:,::1] p_i = np.zeros((nTheta, nBinsMax), dtype=DTYPE)
+    cdef double[:,::1] lambda_i = np.zeros((nTheta, nBins), dtype=DTYPE)
 
     for i in range(nTheta):
         # Check theta has the correct length
@@ -76,34 +81,38 @@ def log_like(pt_sum_compressed, theta, f_ary, df_rho_div_f_ary, npt_compressed,
 
         x_m_ary_out, x_m_sum_out = x_m.return_xs(theta[i,:-3].astype(DTYPE), f_ary, df_rho_div_f_ary, 
                                        npt_compressed[i], data_sum)
-        x_m_ary += np.asarray(x_m_ary_out)
-        x_m_sum += np.asarray(x_m_sum_out)
 
-        nBins[i] = len(theta[i,-3]) - 1
-        norm[i] = integrate.quad( lambda x: theta[i,-2](x,theta[i,-1]), theta[i,-3][0], theta[i,-3][nBins[i]] )[0]
-        for b in range(nBins[i]):
-            p_i[i,b] = integrate.quad( lambda x: theta[i,-2](x,theta[i,-1]), theta[i,-3][b], theta[i,-3][b+1] )[0]/norm[i]
+        x_m_ary[i] = x_m_ary_out
+        x_m_sum[i] = np.asarray(x_m_sum_out)
+
+        norm[i] = integrate.quad( lambda x: theta[i,-2](x,theta[i,-1]), theta[i,-3][0], theta[i,-3][nBins] )[0]
+        for b in range(nBins):
+            lambda_i[i,b] = integrate.quad( lambda x: theta[i,-2](x,theta[i,-1]), theta[i,-3][b], theta[i,-3][b+1] )[0]/norm[i]
 
     return log_like_internal(pt_sum_compressed, data, data_sum, x_m_ary, x_m_sum, k_max,
-                             npixROI, p_i[0,:])
+                             npixROI, lambda_i)
 
 @cython.boundscheck(False)
 @cython.wraparound(False)
 @cython.cdivision(True)
 @cython.initializedcheck(False)
-cdef double MultiNom(int[:] data,int data_sum,double[::1] p_i):
-    cdef double res = factorial(data_sum)
-    for i in range(len(p_i)):
-        res *= p_i[i]**data[i] / factorial(data[i])
+cdef double MultiNomCoeff(int[:] comb):
+    cdef Py_ssize_t a,j
+    cdef double res=1, i = 1
+    for a in range(len(comb)):
+        for j in range(1,comb[a]+1):
+            res *= i
+            res //= j
+            i += 1
     return res 
- 
+
 @cython.boundscheck(False)
 @cython.wraparound(False)
 @cython.cdivision(True)
 @cython.initializedcheck(False)
 cdef double log_like_internal(double[::1] pt_sum_compressed, int[:,::1] data, int[::1] data_sum,
-                              double[:,::1] x_m_ary, double[::1] x_m_sum, 
-                              int k_max, int npixROI, double[::1] p_i):
+                              double[:,:,::1] x_m_ary, double[:,::1] x_m_sum, 
+                              int k_max, int npixROI, double[:,::1] lambda_i):
     """ Calculation of log likelihood
 
     Take the x_m and x_m_sum values, which represent the information for the
@@ -117,34 +126,69 @@ cdef double log_like_internal(double[::1] pt_sum_compressed, int[:,::1] data, in
         double log likelihood 
 
     """
+    cdef int nTemp = len(x_m_sum)
+    cdef int nBins = len(lambda_i[0])
 
-    cdef Py_ssize_t p, k, n
+    cdef Py_ssize_t p=0, k=0, n=0, i=0, j=0, t=0, k1=0
     cdef double ll = 0.
-    cdef double f0_ary, f1_ary
-    cdef double[:] pk = np.zeros((k_max+1), dtype=DTYPE)
+    cdef double f0_ary=0, f1_ary=0
+    cdef double[:,::1] pk = np.zeros((nTemp,k_max+1), dtype=DTYPE)
 
+    cdef int[::1] data_loc = np.zeros(nTemp, dtype=np.int32)
+    cdef int data_sum_loc = 0
+    cdef double term = 0.
+    cdef double term_comb = 0.
+    
+    cdef int[::1] comb_off = np.zeros(k_max+1, dtype=np.int32)
+    cdef int[::1] comb_len = np.zeros(k_max+1, dtype=np.int32)
+
+    comb_len[0] = 1
+    for n in range(k_max):
+        comb_len[n+1] = <int>binom(nTemp+n,nTemp-1)
+        comb_off[n+1] = comb_off[n] + comb_len[n+1] -1
+
+    cdef int[:,::1] comb_list = np.zeros((comb_off[k_max]+comb_len[k_max],nTemp),dtype=np.int32)
+    for n in range(k_max+1):
+        for k1 in range(n+1):
+            comb_list[comb_off[n]+k1][0] = k1
+            comb_list[comb_off[n]+k1][1] = n-k1
+
+    cdef double[:,:,::1] powLambda = np.zeros((nTemp,nBins,k_max+1), dtype=DTYPE)
+    for t in range(nTemp):
+        for n in range(nBins):
+            for k in range(k_max+1):
+                powLambda[t][n][k] = lambda_i[t][n]**k 
+                
     # Loop over pixels
     for p in range(npixROI):
-        # Define p_0 (pk[0]) and p_1 (pk[1])
-        # Then the remaining p_k are determined recursively up to the value of 
-        # k = data in that pixel
-        f0_ary = -(pt_sum_compressed[p] + x_m_sum[p])
-        f1_ary = (pt_sum_compressed[p] + x_m_ary[p,1])
-        pk[0] = exp(f0_ary)
-        pk[1] = pk[0] * f1_ary
+        for t in range(nTemp):
+            # Define p_0 (pk[0]) and p_1 (pk[1])
+            # Then the remaining p_k are determined recursively up to the value of 
+            # k = data in that pixel
+            f0_ary = -(pt_sum_compressed[p] + x_m_sum[t,p])
+            f1_ary = (pt_sum_compressed[p] + x_m_ary[t,p,1])
+            pk[t][0] = exp(f0_ary)
+            pk[t][1] = pk[t][0] * f1_ary
 
-        for k in range(2, data_sum[p]+1):
-            pk[k] = 0.
-            for n in range(0, k-1):
-                pk[k] += (k-n)/float(k)*x_m_ary[p,k-n]*pk[n]
-            pk[k] += f1_ary*pk[k-1]/float(k)
+            for k in range(2, k_max+1):
+                pk[t][k] = 0.
+                for n in range(0, k-1):
+                    pk[t][k] += (k-n)/float(k)*x_m_ary[t,p,k-n]*pk[t][n]
+                pk[t][k] += f1_ary*pk[t][k-1]/float(k)
 
-        # Need prob > 0 to define a LL
-        # If a very bad fit can get prob = 0, if so then penalise to avoid this
-        # region
-        if pk[data_sum[p]] > 0:
-            ll += log( MultiNom(data[:,p],data_sum[p],p_i) * pk[data_sum[p]] )
-        else:
-            ll += -10.1**10.
+        term_comb = 0
+        for i in range(comb_len[data[0,p]]):
+            for j in range(comb_len[data[1,p]]):
+                term = 1
+                for t in range(nTemp):
+                    data_loc[0] = comb_list[comb_off[data[0,p]]+i][t]
+                    data_loc[1] = comb_list[comb_off[data[1,p]]+j][nTemp-t-1]
+                    data_sum_loc = sum(data_loc) 
+
+                    term *=  MultiNomCoeff(data_loc) * pk[t][data_sum_loc]
+                    for k in range(nBins):
+                        term *= powLambda[t][k][data_loc[k]]
+                term_comb += term
+        ll += log(term_comb)
 
     return ll
